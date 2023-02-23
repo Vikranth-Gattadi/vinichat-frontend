c30adf923507278e2c83ad49f8e1f801214a7293
Vikranth Gattadi <vikranthviky143@gmail.com>
2023-02-23 18:22:24 +0530

    third yo

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://vinichat-api.onrender.com"
+    baseURL:  "https://vinichat-api.onrender.com/"
 })
 
 export default instance;
